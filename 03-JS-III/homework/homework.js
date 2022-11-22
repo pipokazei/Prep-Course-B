a6f9c6c89018cdddf3eec5cf5634842ffbbb885e
Zeynep Elif Tarkan <pipoka@Zeyneps-MacBook-Pro.local>
2022-11-22 18:55:22 -0300

    tarea 03

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,54 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var suma = numeros.reduce(
+      (acc, currentValue) => acc + currentValue
+    );
+    return suma;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var pares = [];
+  for (var i = 0; i < array.length; i++){
+    if (array[i] % 2 === 0) {
+      pares.push(array[i]);
+    }
+  }
+  return pares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var cuadrado = array.map( 
+    (elemento) => elemento ** 2
+  ); 
+  return cuadrado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var total = 0;
+  for (var i = 0; i < array.length; i++) {
+    total = total + array[i];
+  }
+  return total;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length;
+  
+
+  
 }
   
   
